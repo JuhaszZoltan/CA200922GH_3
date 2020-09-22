d129034b1eb75c6a0c46b6e26cdea7610493f42c
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-09-22 12:18:26 +0200

    Add project files.

---
+++ CA200922GH_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200922GH_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  x y 
+            //1 1 2 3 5 8 13 21 
+            int n = 30;
+            int x = 1;
+            int y = 1;
+            Console.Write($"{x} {y} ");
+            for (int i = 0; i < n; i++)
+            {
+                int k = x + y;
+                Console.Write($"{k} ");
+                x = y;
+                y = k;
+            }
+            Console.ReadKey();
+        }
+    }
+}
